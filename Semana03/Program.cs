eb691f9966e9cd97546003fcf03324b3e8675549
Erwin <erwinsito2003@gmail.com>
2025-04-10 11:50:27 -0500

    Ejercicio01

---
+++ Semana03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("/n"+nombre+ " ,Bienvenido al curso de Fundamentos de algoritmos");
+            Console.ReadKey();
+
+
+        }
+    }
+}
